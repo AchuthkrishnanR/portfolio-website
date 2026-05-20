1842c51f011ad19dbde435c0402eb099f5959620
Achuth Krishnan <achuthkrishnanak@gmail.com>
2026-05-20 17:02:04 +0530

    Added scroll reveal animations

--- script.js
+++ script.js
@@ -36,3 +36,40 @@ function type() {
 }
 
 type();
+/* SCROLL REVEAL ANIMATION */
+
+ScrollReveal().reveal('.hero-content', {
+    delay: 200,
+    distance: '50px',
+    origin: 'bottom',
+    duration: 1000
+});
+
+ScrollReveal().reveal('.about', {
+    delay: 200,
+    distance: '50px',
+    origin: 'left',
+    duration: 1000
+});
+
+ScrollReveal().reveal('.skills', {
+    delay: 200,
+    distance: '50px',
+    origin: 'right',
+    duration: 1000
+});
+
+ScrollReveal().reveal('.project-card', {
+    delay: 200,
+    interval: 200,
+    distance: '50px',
+    origin: 'bottom',
+    duration: 1000
+});
+
+ScrollReveal().reveal('.contact', {
+    delay: 200,
+    distance: '50px',
+    origin: 'bottom',
+    duration: 1000
+});
